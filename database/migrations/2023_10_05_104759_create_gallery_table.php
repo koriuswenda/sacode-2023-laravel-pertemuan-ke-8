ed5e5a6b610e8011cdde98e1051d3c8d85d5bf6c
korius kussa wenda <koriuswenda@gmail.com>
2023-10-05 17:58:52 +0700

    initial commit

--- database/migrations/2023_10_05_104759_create_gallery_table.php
+++ database/migrations/2023_10_05_104759_create_gallery_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('gallery', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('facebook')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+
+                $table->string('cover');
+                $table->string('title');
+                $table->text('descriptsi')->nullable();
+                $table->enum('status', ['public','hidden']);
+
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('gallery');
     }
 };
